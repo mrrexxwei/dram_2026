29c0cdc2a98e709dd868f526568615503ac1f9f6
Mr. Rexx <kweirachmaciej@pittsfield.net>
2026-06-07 12:22:44 -0400

    2026 Version of Dram Game

---
+++ script.js
@@ -0,0 +1,108 @@
+// this file will add event listener to the pictures/buttons. When click on the buttons, a sound will be played
+
+let letters = ['a', 'w', 's', 'd', 'j', 'k', 'l'];
+
+//get the number of the drum elements from HTML page
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+let keysPressedAll = "";
+
+// Task 2: add event listener to all drum pictures, so while CLICK the image, 
+// the function LINKSOUND will be called, you can do it with a for loop
+// ------ write your code this line ------
+
+
+
+// ------ end of your code ------
+
+
+// link the sound to the element
+function linkSound() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    if (letters.includes(buttonInnerHTML)) {
+      updateRecord(buttonInnerHTML);
+    }
+    
+}
+
+// add event listener to the document element. Listen to any key press event. 
+// if any of w, a, s, d, j, k or l is pressed, a sound will be display too.
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  if (letters.includes(event.key)) {
+    updateRecord(event.key);
+  }
+
+});
+
+//a fucntion can make sound, use key as a pass-in peremiter
+// the program will play different sound base on the key pass in
+// mp3 files are in the "sounds" folder, just find a way to play it
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      // path to the audio file is "folderName/fileName"
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      // updateRecord(key);
+      break;
+    default: console.log(key);
+  }
+}
+
+//control the playback speed of record
+function waitMoments() {
+  return new Promise(resolve => setTimeout(resolve, 280)); // 1000 ms = 1 second
+}
+
+
+function updateRecord(key) {
+  let keysPressed = document.querySelector("#keys-pressed");
+  // keysPressed.innerHTML = keysPressed.innerHTML + key; 
+  keysPressedAll += key;
+  keysPressed.innerHTML = "You hitted: " + keysPressedAll.slice(-12).toUpperCase();
+}
+
+
+async function playRecord() {
+  // let keysRec = document.querySelector("#keys-pressed").innerHTML;
+  let keysRec = keysPressedAll;
+  console.log(keysRec);
+  for (let i = 0; i < keysRec.length; i++) {
+    console.log(keysRec[i]);
+    makeSound(keysRec[i]);
+    await waitMoments();
+  }
+}
+
